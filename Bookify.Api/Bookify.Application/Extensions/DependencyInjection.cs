d8302bee86cc88985f4e09cbcd34d357007a9928
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-20 18:06:31 +0500

    Minor update

--- Bookify.Api/Bookify.Application/Extensions/DependencyInjection.cs
+++ Bookify.Api/Bookify.Application/Extensions/DependencyInjection.cs
@@ -1,7 +1,10 @@
 ﻿using Bookify.Application.Interfaces;
 using Bookify.Application.Services;
+using Bookify.Application.Validations.Auth;
+using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 namespace Bookify.Application.Extensions;
 
@@ -9,6 +12,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection RegisterApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
+        services.AddFluentValidationAutoValidation();
+
         services.AddSingleton<ICurrentUserService, CurrentUserService>();
 
         services.AddScoped<IAuthService, AuthService>();
