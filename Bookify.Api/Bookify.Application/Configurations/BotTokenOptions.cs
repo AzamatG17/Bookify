b87a8683e99a24ea26070fe7d59e6737df7c7637
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-20 17:58:45 +0500

    Add Telegram bot option

---
+++ Bookify.Api/Bookify.Application/Configurations/BotTokenOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookify.Application.Configurations;
+
+public sealed class BotTokenOptions
+{
+    public const string SectionName = nameof(BotTokenOptions);
+
+    [Required]
+    public required string TelegramToken { get; init; }
+}
