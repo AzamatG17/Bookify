5e3a0ce49b01b3b13c8ea89d7f0e685511f7debe
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-21 17:04:00 +0500

    Add Api client

---
+++ Bookify.Api/Bookify.Infrastructure/Client/ApiClient.cs
@@ -0,0 +1,46 @@
+﻿using Bookify.Application.Interfaces;
+using System.Text.Json;
+
+namespace Bookify.Infrastructure.Client;
+
+internal sealed class ApiClient : IApiClient
+{
+    private readonly HttpClient _client;
+
+    public ApiClient(HttpClient client)
+    {
+        _client = client ?? throw new ArgumentNullException(nameof(client));
+
+    }
+
+    public async Task<TResult> GetAsync<TResult>(string url)
+    {
+        if (string.IsNullOrEmpty(url))
+            throw new ArgumentNullException(nameof(url));
+
+        var response = await _client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<TResult>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        }) ?? throw new InvalidOperationException("Deserialization returned null.");
+    }
+
+    public Task<TResult> PostAsync<TResult, TRequest>(string url, TRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PutAsync<TRequest>(string url, TRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(string url)
+    {
+        throw new NotImplementedException();
+    }
+}
