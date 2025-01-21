a4b743025c62559ec903d26bb8524496e884ecd8
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-22 00:33:15 +0500

    Create Company Requests

---
+++ Bookify.Api/Bookify.Application/Requests/Services/CreateCompanyRequest.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain_.Enums;
+
+namespace Bookify.Application.Requests.Services;
+
+public record CreateCompanyRequest(
+    string Name,
+    string BaseUrl,
+    Projects Projects,
+    string Color,
+    string BackgroundColor
+    );
