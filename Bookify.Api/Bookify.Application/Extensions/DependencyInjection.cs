b52448a1b080d0ac5d9faf69eae96060f7fd4d5b
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-22 00:34:23 +0500

    Register for DI

--- Bookify.Api/Bookify.Application/Extensions/DependencyInjection.cs
+++ Bookify.Api/Bookify.Application/Extensions/DependencyInjection.cs
@@ -1,4 +1,7 @@
 ﻿using Bookify.Application.Interfaces;
+using Bookify.Application.Interfaces.IServices;
+using Bookify.Application.Interfaces.Services;
+using Bookify.Application.Mappings;
 using Bookify.Application.Services;
 using Bookify.Application.Validations.Auth;
 using FluentValidation;
@@ -12,11 +15,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection RegisterApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddAutoMapper(typeof(CompaniesMappings).Assembly);
         services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
         services.AddFluentValidationAutoValidation();
 
         services.AddSingleton<ICurrentUserService, CurrentUserService>();
 
+        services.AddScoped<ICompaniesService, CompaniesService>();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ISmsCodeService, SmsCodeService>();
         services.AddSingleton<IJwtTokenHandler, JwtTokenHandler>();
---
+++ Bookify.Api/Bookify.Application/Requests/Services/UpdateCompanyRequest.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain_.Enums;
+
+namespace Bookify.Application.Requests.Services;
+
+public record UpdateCompanyRequest(
+    int Id,
+    string Name,
+    string BaseUrl,
+    Projects Projects,
+    string Color,
+    string BackgroundColor
+    );
