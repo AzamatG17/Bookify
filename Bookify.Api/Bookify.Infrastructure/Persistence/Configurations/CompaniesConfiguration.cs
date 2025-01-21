ac14c20361ccde6b850324b3a014fbaa1ecd8023
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-21 17:05:33 +0500

    Add Configuration for new entities

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Configurations/CompaniesConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Bookify.Domain_.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Persistence.Configurations;
+
+internal class CompaniesConfiguration : IEntityTypeConfiguration<Companies>
+{
+    public void Configure(EntityTypeBuilder<Companies> builder)
+    {
+        builder.ToTable(nameof(Companies));
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(c => c.BaseUrl)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.Property(c => c.Projects)
+            .IsRequired();
+
+        builder.Property(c => c.Color)
+            .HasMaxLength(50);
+
+        builder.Property(c => c.BackgroundColor)
+            .HasMaxLength(50);
+    }
+}
---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain_.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Persistence.Configurations;
+
+public class ServicesConfiguration : IEntityTypeConfiguration<Services>
+{
+    public void Configure(EntityTypeBuilder<Services> builder)
+    {
+        builder.ToTable(nameof(Services));
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Name)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.Property(s => s.Description)
+            .HasMaxLength(1000);
+
+        builder.Property(s => s.Location)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
