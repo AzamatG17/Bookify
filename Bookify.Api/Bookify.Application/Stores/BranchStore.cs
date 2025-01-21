28d60d208618967177ab4bc3c250cf50763fc209
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-21 17:06:51 +0500

    Minor update

---
+++ Bookify.Api/Bookify.Application/Stores/BranchStore.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Application.Interfaces;
+using Bookify.Application.Interfaces.IStores;
+using Bookify.Application.Models;
+
+namespace Bookify.Application.Stores;
+
+internal sealed class BranchStore(IApiClient apiClient) : IBranchStore
+{
+    private readonly IApiClient _apiClient = apiClient ?? throw new ArgumentNullException(nameof(apiClient));
+
+    public async Task<List<Branches>> GetAllAsync(string baseUrl)
+    {
+        if (string.IsNullOrEmpty(baseUrl))
+            throw new ArgumentNullException(nameof(baseUrl));
+
+        var endpoint = $"{baseUrl}/api/Branches/ListBranches";
+        return await _apiClient.GetAsync<List<Branches>>(endpoint);
+    }
+}
