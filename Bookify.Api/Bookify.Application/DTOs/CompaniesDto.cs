4398904696db4a51e8745a4c9ece3e43014021ef
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-22 00:32:54 +0500

    Create Company Dto

---
+++ Bookify.Api/Bookify.Application/DTOs/CompaniesDto.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain_.Enums;
+
+namespace Bookify.Application.DTOs;
+
+public record CompaniesDto(
+    int Id,
+    string Name,
+    string BaseUrl,
+    Projects Projects,
+    string? Color,
+    string? BackgroundColor
+    );
