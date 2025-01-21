d7d655d54a8724e404c559ce00c2f7b929a4326d
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-21 17:05:10 +0500

    Add new entities

---
+++ Bookify.Api/Bookify.Domain'/Entities/Companies.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain_.Common;
+using Bookify.Domain_.Enums;
+
+namespace Bookify.Domain_.Entities;
+
+public class Companies : AuditableEntity
+{
+    public required string Name { get; set; }
+    public required string BaseUrl { get; set; }
+    public required Projects Projects { get; set; }
+    public string? Color { get; set; }
+    public string? BackgroundColor { get; set; }
+}
---
+++ Bookify.Api/Bookify.Domain'/Enums/Projects.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain_.Enums;
+
+public enum Projects
+{
+    BookingService = 1,
+    Onlinet = 2
+}
