65346a14adab93777d2525dc8fe54f51b036850d
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-22 17:58:15 +0500

    Generate Services

--- Bookify.Api/Bookify.Application/Interfaces/Services/IBranchService.cs
+++ Bookify.Api/Bookify.Application/Interfaces/Services/IBranchService.cs
@@ -1,8 +1,10 @@
-﻿using Bookify.Application.Models;
+﻿using Bookify.Application.DTOs;
+using Bookify.Application.Requests.Services;
 
 namespace Bookify.Application.Interfaces.IServices;
 
 public interface IBranchService
 {
-    Task<List<Branches>> GetAllAsync();
+    Task<List<CompanyWithBranchesDto>> GetAllAsync();
+    Task<CompanyWithBranchesDto> GetByIdAsync(CompanyRequest companyRequest);
 }
--- Bookify.Api/Bookify.Application/Services/BranchService.cs
+++ Bookify.Api/Bookify.Application/Services/BranchService.cs
@@ -1,11 +1,15 @@
-﻿using Bookify.Application.Interfaces.IStores;
+﻿using Bookify.Application.DTOs;
+using Bookify.Application.Interfaces.IServices;
+using Bookify.Application.Interfaces.IStores;
 using Bookify.Application.Models;
+using Bookify.Application.Requests.Services;
+using Bookify.Domain_.Exceptions;
 using Bookify.Domain_.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Application.Services;
 
-internal sealed class BranchService
+internal sealed class BranchService : IBranchService
 {
     private readonly IApplicationDbContext _context;
     private readonly IBranchStore _branchStore;
@@ -16,17 +20,69 @@ internal sealed class BranchService
         _branchStore = branchStore ?? throw new ArgumentNullException(nameof(branchStore));
     }
 
-    public async Task<List<Branches>> GetAllAsync()
+    public async Task<List<CompanyWithBranchesDto>> GetAllAsync()
     {
-        var companies = await _context.Companies.ToListAsync();
+        var companies = await _context.Companies
+            .AsNoTracking()
+            .ToListAsync();
 
-        List<Branches> result = [];
+        var result = new List<CompanyWithBranchesDto>();
+
         foreach (var company in companies)
         {
-            var branches = await _branchStore.GetAllAsync(company.BaseUrl);
-            result.AddRange(branches);
+            List<Branches>? branches = null;
+
+            if (company.Projects == Domain_.Enums.Projects.BookingService)
+            {
+                branches = await _branchStore.GetAllAsync(company.BaseUrl);
+            }
+            else if (company.Projects == Domain_.Enums.Projects.Onlinet)
+            {
+                branches = await _branchStore.GetAllForOnlinetAsync(company.BaseUrl);
+            }
+
+            result.Add(new CompanyWithBranchesDto(
+                Id: company.Id,
+                Name: company.Name,
+                Projects: company.Projects,
+                Color: company.Color,
+                BackgroundColor: company.BackgroundColor,
+                Branches: branches
+            ));
         }
 
         return result;
+    }
+
+    public async Task<CompanyWithBranchesDto> GetByIdAsync(CompanyRequest request)
+    {
+        var company = await _context.Companies
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == request.Id);
+
+        if (company is null)
+        {
+            throw new EntityNotFoundException($"Company with id:{request.Id} does not exist.");
+        }
+
+        List<Branches> branches = new();
+
+        if (company.Projects == Domain_.Enums.Projects.BookingService)
+        {
+            branches = await _branchStore.GetAllAsync(company.BaseUrl);
+        }
+        else if (company.Projects == Domain_.Enums.Projects.Onlinet)
+        {
+            branches = await _branchStore.GetAllForOnlinetAsync(company.BaseUrl);
+        }
+
+        return new CompanyWithBranchesDto(
+            Id: company.Id,
+            Name: company.Name,
+            Projects: company.Projects,
+            Color: company.Color,
+            BackgroundColor: company.BackgroundColor,
+            Branches: branches
+        );
     }
 }
