d082480e555cc7007ef016c0c49f56ae90c04afd
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-23 00:06:14 +0500

    Add mappper and Service for Branch

---
+++ Bookify.Api/Bookify.Application/Mappings/BranchMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Bookify.Application.Models;
+using Bookify.Domain_.Entities;
+
+namespace Bookify.Application.Mappings;
+
+public class BranchMapper : Profile
+{
+    public BranchMapper()
+    {
+        CreateMap<Branches, Branch>();
+        CreateMap<Branch, Branches>();
+    }
+}
