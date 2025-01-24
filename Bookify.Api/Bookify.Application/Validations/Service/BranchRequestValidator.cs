27f87f726b1b711676e0eba552da4d8a51e71373
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-24 18:15:10 +0500

    Add validations

---
+++ Bookify.Api/Bookify.Application/Validations/Service/BranchRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Application.Requests.Services;
+using FluentValidation;
+
+namespace Bookify.Application.Validations.Service;
+
+public class BranchRequestValidator : AbstractValidator<BranchRequest>
+{
+    public BranchRequestValidator()
+    {
+        RuleFor(x => x.BranchId)
+            .GreaterThan(0)
+            .WithMessage("BranchId must be greater than 0.");
+    }
+}
---
+++ Bookify.Api/Bookify.Application/Validations/Service/UpdateCompanyRequestValidator.cs
@@ -0,0 +1,40 @@
+﻿using Bookify.Application.Requests.Services;
+using FluentValidation;
+
+namespace Bookify.Application.Validations.Service;
+
+public class UpdateCompanyRequestValidator : AbstractValidator<UpdateCompanyRequest>
+{
+    public UpdateCompanyRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("CompanyId must be greater than 0.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MaximumLength(100)
+            .WithMessage("Name must not exceed 100 characters.");
+
+        RuleFor(x => x.BaseUrl)
+            .NotEmpty()
+            .WithMessage("BaseUrl is required.");
+
+        RuleFor(x => x.Projects)
+            .NotNull()
+            .WithMessage("Projects cannot be null.");
+
+        RuleFor(x => x.Color)
+            .NotEmpty()
+            .WithMessage("Color is required.")
+            .Matches("^#[0-9a-fA-F]{6}$")
+            .WithMessage("Color must be a valid hex color code (e.g., #FFFFFF).");
+
+        RuleFor(x => x.BackgroundColor)
+            .NotEmpty()
+            .WithMessage("BackgroundColor is required.")
+            .Matches("^#[0-9a-fA-F]{6}$")
+            .WithMessage("BackgroundColor must be a valid hex color code (e.g., #FFFFFF).");
+    }
+}
