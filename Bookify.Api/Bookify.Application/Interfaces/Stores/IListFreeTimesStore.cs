2159702aeeb08f2439e51f2638c46cdc996e31e0
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-28 17:27:04 +0500

    minor update

---
+++ Bookify.Api/Bookify.Application/Interfaces/Stores/IListFreeTimesStore.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Application.DTOs;
+
+namespace Bookify.Application.Interfaces.Stores;
+
+public interface IListFreeTimesStore
+{
+    Task<List<FreeTimeDto>> GetDataBookingServiceAsync(int branchId, int serviceId, DateTime startDate, string baseUrl);
+    Task GetDataOnlinetAsync(int branchId, int serviceId, DateTime startDate);
+}
