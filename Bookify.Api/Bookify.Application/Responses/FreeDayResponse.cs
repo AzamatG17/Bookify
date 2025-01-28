6d45794009c819b21712513c5ea225d4ed2d9ff3
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-29 00:16:01 +0500

    create response

---
+++ Bookify.Api/Bookify.Application/Responses/FreeDayResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Application.Responses;
+
+public class FreeDayResponse
+{
+    public string Date { get; set; }
+    public int Day { get; set; } 
+    public int FreePlacesCount { get; set; }
+    public int Month { get; set; }
+    public int TotalPlacesCount { get; set; }
+    public int Year { get; set; }
+}
---
+++ Bookify.Api/Bookify.Application/Responses/FreeTimeOnlinetResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookify.Application.Responses;
+
+public class FreeTimeOnlinetResponse
+{
+    public int FreePlacesCount { get; set; }
+    public int Hour { get; set; }
+    public int Minute { get; set; }
+    public int Second { get; set; }
+    public string Time { get; set; }
+    public int TotalPlacesCount { get; set; }
+}
