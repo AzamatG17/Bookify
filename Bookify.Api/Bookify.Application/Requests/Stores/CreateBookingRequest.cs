510f106b8e44eabb6556caa78a94319c18c0ce96
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-29 17:53:49 +0500

    Create Booking Request

---
+++ Bookify.Api/Bookify.Application/Requests/Stores/CreateBookingRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Bookify.Application.Requests.Stores;
+
+public class CreateBookingRequest
+{
+    public string BranchId { get; set; }
+    public string CustomerID { get; set; } = "";
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LanguageShortId { get; set; }
+    public string LastName { get; set; }
+    public string Name { get; set; }
+    public string Note { get; set; } = "";
+    public string PhoneNumber { get; set; }
+    public string ServiceId { get; set; }
+    public string StartDate { get; set; } // формат "yyyyMMdd"
+    public string StartTime { get; set; } // формат "HH:mm:ss"
+}
