56b7a4f8a812a45ec7bd983008806ffad65c44b6
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-29 16:01:31 +0500

    Add Validator for FreeTime request

---
+++ Bookify.Api/Bookify.Application/Validations/Service/FreeTimeRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Application.Requests.Services;
+using FluentValidation;
+
+namespace Bookify.Application.Validations.Service;
+
+public class FreeTimeRequestValidator : AbstractValidator<FreeTimeRequest>
+{
+    public FreeTimeRequestValidator()
+    {
+        RuleFor(x => x.ServiceId)
+            .GreaterThan(0).WithMessage("ServiceId must be greater than 0.");
+
+        RuleFor(x => x.DateOnly)
+            .GreaterThan(DateTime.Now.Date.AddDays(-1)) 
+            .WithMessage("Date must be in the future.");
+    }
+}
