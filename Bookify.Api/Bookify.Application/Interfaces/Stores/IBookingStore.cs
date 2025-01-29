8b1013d746c0df623baa4fb9113ea87f82cdde92
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-01-30 00:08:51 +0500

    Create Booking store

---
+++ Bookify.Api/Bookify.Application/Interfaces/Stores/IBookingStore.cs
@@ -0,0 +1,10 @@
+﻿using Bookify.Application.Requests.Stores;
+using Bookify.Application.Responses;
+
+namespace Bookify.Application.Interfaces.Stores;
+
+public interface IBookingStore
+{
+    Task<CreateBookingResponse> CreateBookingForBookingServiceAsync(BookingRequest request, string baseUrl);
+    Task<CreateBookingResponse> CreateBookingOnlinetAsync(BookingRequest request, string baseUrl);
+}
--- Bookify.Api/Bookify.Application/Stores/BookingStore.cs
+++ Bookify.Api/Bookify.Application/Stores/BookingStore.cs
@@ -1,10 +1,11 @@
 ﻿using Bookify.Application.Interfaces;
+using Bookify.Application.Interfaces.Stores;
 using Bookify.Application.Requests.Stores;
 using Bookify.Application.Responses;
 
 namespace Bookify.Application.Stores;
 
-public class BookingStore
+public class BookingStore : IBookingStore
 {
     private readonly IApiClient _client;
 
@@ -13,26 +14,26 @@ public class BookingStore
         _client = client ?? throw new ArgumentNullException(nameof(client));
     }
 
-    public async Task<CreateBookingResponse> CreateBookingForBookingServiceAsync(CreateBookingRequest request, string baseUrl)
+    public async Task<CreateBookingResponse> CreateBookingForBookingServiceAsync(BookingRequest request, string baseUrl)
     {
         if (string.IsNullOrEmpty(baseUrl))
             throw new ArgumentNullException(nameof(baseUrl));
 
         var endpoint = $"{baseUrl}/api/Bookings/CreateBooking";
 
-        var response = await _client.PostAsync<CreateBookingResponse, CreateBookingRequest>(endpoint, request);
+        var response = await _client.PostAsync<CreateBookingResponse, BookingRequest>(endpoint, request);
 
         return response;
     }
 
-    public async Task<CreateBookingResponse> CreateBookingOnlinetAsync(CreateBookingRequest request, string baseUrl)
+    public async Task<CreateBookingResponse> CreateBookingOnlinetAsync(BookingRequest request, string baseUrl)
     {
         if (string.IsNullOrEmpty(baseUrl))
             throw new ArgumentNullException(nameof(baseUrl));
 
         var endpoint = $"{baseUrl}/easyq/BookingMediator/Service1.svc/json/CreateBooking";
 
-        var response = await _client.PostAsync<CreateBookingResponse, CreateBookingRequest>(endpoint, request);
+        var response = await _client.PostAsync<CreateBookingResponse, BookingRequest>(endpoint, request);
 
         return response;
     }
