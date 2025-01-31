cf7b32cabf056a9a3a215943339e3d1f3ac2244b
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-31 18:04:39 +0500

    Add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250131072413_FixStartTimeType.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixStartTimeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("fa472cfa-5d4e-4149-8dc7-bb86cf5157b2"));
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("0ee63ebc-c821-4dfa-903f-bb4915452ac7"), new Guid("ecb1b6d9-bcac-46ac-b6e2-ad210f0b5713") });
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("0ee63ebc-c821-4dfa-903f-bb4915452ac7"));
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("ecb1b6d9-bcac-46ac-b6e2-ad210f0b5713"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Booking",
+                type: "datetime2",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), null, "admin", "ADMIN" },
+                    { new Guid("b17395de-ad6c-43ba-9059-70bdc80be587"), null, "user", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "AccessFailedCount", "ChatId", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506"), 0, 0L, "2ce09139-4249-4464-9427-2e2c546c92e9", "admin@example.com", true, "admin", "admin", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAEHz7NcGdnGuFGbNE6+51o+7vritld2HPs97zr9qfH1wWU+vO6YG6Oajash9DJG1rSA==", null, false, null, false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("b17395de-ad6c-43ba-9059-70bdc80be587"));
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506") });
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"));
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartDate",
+                table: "Booking",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldMaxLength: 20);
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("0ee63ebc-c821-4dfa-903f-bb4915452ac7"), null, "admin", "ADMIN" },
+                    { new Guid("fa472cfa-5d4e-4149-8dc7-bb86cf5157b2"), null, "user", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "AccessFailedCount", "ChatId", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("ecb1b6d9-bcac-46ac-b6e2-ad210f0b5713"), 0, 0L, "77471469-dad9-4519-abd4-9d7e1dfca6ea", "admin@example.com", true, "admin", "admin", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAEBnxlU4xi8P2YNlw5JnExW1m8j6jEZz1GPv2rCJ0EpmKOlRpDbKnlb2G76pHPtEdXw==", null, false, null, false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("0ee63ebc-c821-4dfa-903f-bb4915452ac7"), new Guid("ecb1b6d9-bcac-46ac-b6e2-ad210f0b5713") });
+        }
+    }
+}
---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250131125315_Add-ETicket-table.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddETickettable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("b17395de-ad6c-43ba-9059-70bdc80be587"));
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506") });
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"));
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506"));
+
+            migrationBuilder.CreateTable(
+                name: "ETicket",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceId = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ServiceName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    BranchName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedTime = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Success = table.Column<bool>(type: "bit", nullable: false),
+                    ShowArriveButton = table.Column<bool>(type: "bit", nullable: false),
+                    ValidUntil = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastUpdatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ETicket", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_ETicket_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("242bd260-6fff-4867-9701-25c340d30e9b"), null, "admin", "ADMIN" },
+                    { new Guid("a2c67c36-9580-4697-abc7-a27b8f5a0153"), null, "user", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "AccessFailedCount", "ChatId", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("ba6e8f9d-4e29-4e1f-9494-2bee7f93b4d5"), 0, 0L, "94a97d43-dd3c-455d-b33a-c68d8f179ded", "admin@example.com", true, "admin", "admin", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAEK3XivNZ0Ic0wCcf/aN0ZOqfqlckoSEcyTY2Ze6dX6BMWbXb7Z2kDEW5o69AJNcl7A==", null, false, null, false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("242bd260-6fff-4867-9701-25c340d30e9b"), new Guid("ba6e8f9d-4e29-4e1f-9494-2bee7f93b4d5") });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ETicket_UserId",
+                table: "ETicket",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ETicket");
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("a2c67c36-9580-4697-abc7-a27b8f5a0153"));
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("242bd260-6fff-4867-9701-25c340d30e9b"), new Guid("ba6e8f9d-4e29-4e1f-9494-2bee7f93b4d5") });
+
+            migrationBuilder.DeleteData(
+                table: "Role",
+                keyColumn: "Id",
+                keyValue: new Guid("242bd260-6fff-4867-9701-25c340d30e9b"));
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("ba6e8f9d-4e29-4e1f-9494-2bee7f93b4d5"));
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), null, "admin", "ADMIN" },
+                    { new Guid("b17395de-ad6c-43ba-9059-70bdc80be587"), null, "user", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "AccessFailedCount", "ChatId", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506"), 0, 0L, "2ce09139-4249-4464-9427-2e2c546c92e9", "admin@example.com", true, "admin", "admin", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAEHz7NcGdnGuFGbNE6+51o+7vritld2HPs97zr9qfH1wWU+vO6YG6Oajash9DJG1rSA==", null, false, null, false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("4b0238f8-ceeb-4896-9366-4f054d136cff"), new Guid("59b3f8de-f59b-4f62-b4cf-fd45034ee506") });
+        }
+    }
+}
