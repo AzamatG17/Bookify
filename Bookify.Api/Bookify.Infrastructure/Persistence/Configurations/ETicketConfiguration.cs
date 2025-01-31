8a3beb8daa76c1b9bc79446b3036b65c4c7f25e6
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-31 18:08:23 +0500

    add configurations for ETicket

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Configurations/ETicketConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using Bookify.Domain_.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Persistence.Configurations;
+
+internal class ETicketConfiguration : IEntityTypeConfiguration<ETicket>
+{
+    public void Configure(EntityTypeBuilder<ETicket> builder)
+    {
+        builder.ToTable(nameof(ETicket));
+        builder.HasKey(c => c.Id);
+
+        builder.HasKey(e => e.TicketId);
+
+        builder.Property(e => e.ServiceId)
+            .IsRequired();
+
+        builder.Property(e => e.Language)
+            .HasMaxLength(10);
+
+        builder.Property(e => e.ServiceName)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(e => e.BranchName)
+            .HasMaxLength(200);
+
+        builder.Property(e => e.CreatedTime)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(e => e.Message)
+            .HasMaxLength(500);
+
+        builder.Property(e => e.Number)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(e => e.ValidUntil)
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Success)
+            .IsRequired();
+
+        builder.Property(e => e.ShowArriveButton)
+            .IsRequired();
+
+        builder.Property(e => e.UserId)
+            .IsRequired();
+
+        builder.HasOne(b => b.User)
+            .WithMany(u => u.ETickets)
+            .HasForeignKey(b => b.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
