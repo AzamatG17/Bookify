f5b1200f93d50f792b3dec41feb50320329f471d
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-31 18:07:44 +0500

    Add ETicket store

---
+++ Bookify.Api/Bookify.Application/Interfaces/Stores/IEticketStore.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Application.Requests.Stores;
+using Bookify.Application.Responses;
+
+namespace Bookify.Application.Interfaces.Stores;
+
+public interface IEticketStore
+{
+    Task<EticketResponse> CreateTicketForBookingServiceAsync(EticketRequest request, string baseUrl);
+}
---
+++ Bookify.Api/Bookify.Application/Stores/EticketStore.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Application.Interfaces;
+using Bookify.Application.Interfaces.Stores;
+using Bookify.Application.Requests.Stores;
+using Bookify.Application.Responses;
+
+namespace Bookify.Application.Stores;
+
+public class EticketStore : IEticketStore
+{
+    private readonly IApiClient _client;
+
+    public EticketStore(IApiClient client)
+    {
+        _client = client ?? throw new ArgumentNullException(nameof(client));
+    }
+
+    public async Task<EticketResponse> CreateTicketForBookingServiceAsync(EticketRequest request, string baseUrl)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(request));
+
+        var endpoint = $"{baseUrl}/api/E_Ticket/CreateNewTicket";
+
+        var response = await _client.PostAsync<EticketResponse, EticketRequest>(endpoint, request);
+
+        return response;
+    }
+}
