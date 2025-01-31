77e17c4858f77bbf965565d7676e9d5d2e1a28e5
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-01-31 18:07:05 +0500

    add response

---
+++ Bookify.Api/Bookify.Application/Responses/EticketResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookify.Application.Responses;
+
+public class EticketResponse
+{
+    public string BranchAddress { get; set; }
+    public int BranchId { get; set; }
+    public string BranchName { get; set; } = null;
+    public int Code { get; set; }
+    public string CreatedTime { get; set; } = null;
+    public string EstimatedWaitingTime { get; set; } = null;
+    public string Message { get; set; } = null;
+    public string Number { get; set; }
+    public string Service { get; set; } = null;
+    public bool ShowArriveButton { get; set; }
+    public bool Success { get; set; }
+    public int TicketId { get; set; }
+    public string ValidUntil { get; set; } = null;
+    public int WaitingCount { get; set; }
+}
