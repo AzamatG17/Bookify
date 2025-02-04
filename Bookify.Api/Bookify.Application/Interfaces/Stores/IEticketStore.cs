befe87ea29d001c3608b21c83ab2026e210f3593
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-02-04 17:58:05 +0500

    update Stores

--- Bookify.Api/Bookify.Application/Interfaces/Stores/IEticketStore.cs
+++ Bookify.Api/Bookify.Application/Interfaces/Stores/IEticketStore.cs
@@ -6,4 +6,5 @@ namespace Bookify.Application.Interfaces.Stores;
 public interface IEticketStore
 {
     Task<EticketResponse> CreateTicketForBookingServiceAsync(EticketRequest request, string baseUrl);
+    Task<EticketResponse> CreateTicketForOnlinetAsync(ETicketOnlinetRequest request, string baseUrl);
 }
