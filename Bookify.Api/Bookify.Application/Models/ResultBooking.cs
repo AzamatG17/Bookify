139e3ec3ba5b050b16f4c3956ba0abce8a7b4182
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-02-07 18:09:48 +0500

    create request and response

---
+++ Bookify.Api/Bookify.Application/Models/ResultBooking.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Application.Models;
+
+public class ResultBooking
+{
+    public int Code { get; set; }
+    public string Message { get; set; }
+    public bool Success { get; set; }
+}
