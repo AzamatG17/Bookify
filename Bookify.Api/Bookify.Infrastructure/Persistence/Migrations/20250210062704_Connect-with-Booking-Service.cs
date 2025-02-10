f0b9051bcc2befc5ad69b514e02db48cf814fd38
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-02-10 17:56:58 +0500

    Add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250210062704_Connect-with-Booking-Service.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConnectwithBookingService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Booking_ServiceId",
+                table: "Booking",
+                column: "ServiceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking",
+                column: "ServiceId",
+                principalTable: "Service",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Booking_ServiceId",
+                table: "Booking");
+        }
+    }
+}
