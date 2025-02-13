2deec085100c171aed702ae727fd017829dc339d
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-02-13 17:58:42 +0500

    add new validatior

---
+++ Bookify.Api/Bookify.Application/Validations/Service/EticketStatusRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Application.Requests.Services;
+using FluentValidation;
+
+namespace Bookify.Application.Validations.Service;
+
+public class EticketStatusRequestValidator : AbstractValidator<EticketStatusRequest>
+{
+    public EticketStatusRequestValidator()
+    {
+        RuleFor(x => x.TicketId)
+            .GreaterThan(0).WithMessage("TicketId must be greater than 0");
+
+        RuleFor(x => x.BranchId)
+            .GreaterThan(0).WithMessage("BranchId must be greater than 0");
+    }
+}
