0fb43ee3a8a00db2d486a5800501ab41c6ad76ca
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-02-24 15:47:54 +0500

    update ETicket store

--- Bookify.Api/Bookify.Application/Interfaces/Stores/IEticketStore.cs
+++ Bookify.Api/Bookify.Application/Interfaces/Stores/IEticketStore.cs
@@ -9,5 +9,6 @@ public interface IEticketStore
     Task<object> GetEtickertStatusBookingServiceAsync(EticketStatusRequest request, string baseUrl);
     Task<EticketResponse> CreateTicketForBookingServiceAsync(EticketRequest request, string baseUrl);
     Task<EticketResponse> CreateTicketForOnlinetAsync(ETicketOnlinetRequest request, string baseUrl);
-    Task<EticketDeleteStatus> DeleteBookingServiceAsync(string baseUrl, int branchId, string number);
+    Task<DeleteResponse> DeleteBookingServiceAsync(string baseUrl, int branchId, string number);
+    Task<DeleteResponse> DeleteOnlinetAsync(string baseUrl, string UserId, string number);
 }
