0aba03392b5eed75b26df3daa9854374f4b5bfe7
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-03-04 17:58:52 +0500

    minor update

---
+++ Bookify.Api/Bookify.Application/Interfaces/Services/IPredefinedTextService.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.DTOs;
+using Bookify.Application.Requests.Services;
+
+namespace Bookify.Application.Interfaces.Services;
+
+public interface IPredefinedTextService
+{
+    Task<List<PredefinedTextDto>> GetAllAsync();
+    Task<PredefinedTextDto> CreateAsync(PredefinedTextForCreateDto textDto);
+    Task<PredefinedTextDto> UpdateAsync(PredefinedTextDto textDto);
+    Task DeleteAsync(PredefinedTextRequest predefinedTextRequest);
+}
