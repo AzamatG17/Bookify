fa70afb4a568758e5a6fabd30857d150d52da2c4
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-03-04 17:56:01 +0500

    Add new Entities

---
+++ Bookify.Api/Bookify.Domain'/Entities/PredefinedText.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain_.Entities;
+
+public class PredefinedText
+{
+    public int Id { get; set; }
+    public required string Text { get; set; }
+}
---
+++ Bookify.Api/Bookify.Domain'/Enums/SmileyRating.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookify.Domain_.Enums;
+
+public enum SmileyRating
+{
+    Excellent = 1,
+    Good = 2,
+    Average = 3,
+    Bad = 4,
+    Terrible = 5
+}
