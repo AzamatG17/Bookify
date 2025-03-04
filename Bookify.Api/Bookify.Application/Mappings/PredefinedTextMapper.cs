868de07df374811a98f5e74e48111688badb6124
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-03-04 17:58:08 +0500

    add new Mappers

---
+++ Bookify.Api/Bookify.Application/Mappings/PredefinedTextMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Bookify.Application.DTOs;
+using Bookify.Domain_.Entities;
+
+namespace Bookify.Application.Mappings;
+
+public class PredefinedTextMapper : Profile
+{
+    public PredefinedTextMapper()
+    {
+        CreateMap<PredefinedText, PredefinedTextDto>();
+        CreateMap<PredefinedTextDto, PredefinedText>();
+        CreateMap<PredefinedTextForCreateDto, PredefinedText>();
+    }
+}
