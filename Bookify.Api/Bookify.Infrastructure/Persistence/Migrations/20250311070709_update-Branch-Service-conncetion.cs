cd398241ff70be1a26ebf35f6b3545db58672d7f
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-03-12 17:00:17 +0500

    add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250311070709_update-Branch-Service-conncetion.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBranchServiceconncetion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Branch_BranchId",
+                table: "Service");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Branch_BranchId",
+                table: "Service",
+                column: "BranchId",
+                principalTable: "Branch",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Branch_BranchId",
+                table: "Service");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Branch_BranchId",
+                table: "Service",
+                column: "BranchId",
+                principalTable: "Branch",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
