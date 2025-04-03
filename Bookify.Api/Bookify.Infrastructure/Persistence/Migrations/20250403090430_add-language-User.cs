d6ab922058f21d2bab7c4943ce828076da535071
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-03 17:40:17 +0500

    add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250403090430_add-language-User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlanguageUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "User",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "User");
+        }
+    }
+}
