812cc7d9fa991a78b2065304e6615ca60e931d6f
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-11 17:58:18 +0500

    add new mapper

---
+++ Bookify.Api/Bookify.Application/Mappings/ServiceGroupMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Bookify.Application.DTOs;
+using Bookify.Application.Requests.Services;
+using Bookify.Domain_.Entities;
+
+namespace Bookify.Application.Mappings;
+
+internal sealed class ServiceGroupMapper : Profile
+{
+    public ServiceGroupMapper()
+    {
+        CreateMap<ServiceGroup, ServiceGroupDto>();
+        CreateMap<ServiceGroupDto, ServiceGroup>();
+        CreateMap<ServiceGroupRequest, ServiceGroup>();
+    }
+}
