a9585299ca8f08614dbba6c2d228174639072de1
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-11 17:56:15 +0500

    Add and update Configurations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Configurations/ServiceGroupConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain_.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Persistence.Configurations;
+
+public class ServiceGroupConfiguration : IEntityTypeConfiguration<ServiceGroup>
+{
+    public void Configure(EntityTypeBuilder<ServiceGroup> builder)
+    {
+        builder.ToTable(nameof(ServiceGroup));
+        builder.HasKey(s => s.Id);
+
+        builder.Property(g => g.Name)
+               .IsRequired()
+               .HasMaxLength(2000);
+    }
+}
