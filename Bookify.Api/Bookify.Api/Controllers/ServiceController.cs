2ea2deb16bbaf602b430b0ef44c6920515f49380
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-15 17:56:08 +0500

    update Controllers

--- Bookify.Api/Bookify.Api/Controllers/ServiceController.cs
+++ Bookify.Api/Bookify.Api/Controllers/ServiceController.cs
@@ -1,6 +1,8 @@
 ﻿using Bookify.Application.DTOs;
 using Bookify.Application.Interfaces.Services;
 using Bookify.Application.QueryParameters;
+using Bookify.Application.Requests.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Api.Controllers;
@@ -17,6 +19,11 @@ public class ServiceController : ControllerBase
         _service = service ?? throw new ArgumentNullException(nameof(service));
     }
 
+    /// <summary>
+    /// Get all Services
+    /// </summary>
+    /// <param name="service"></param>
+    /// <returns></returns>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -26,5 +33,28 @@ public class ServiceController : ControllerBase
         var companies = await _service.GetAllAsync(service);
 
         return Ok(companies);
+    }
+
+    /// <summary>
+    /// Update a Service
+    /// </summary>
+    /// <returns></returns>
+    [HttpPut("{id:int:min(1)}")]
+    [Authorize(Policy = "Admin")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> UpdateServiceAsync([FromRoute] int id, [FromBody] UpdateServiceGroupRequest request)
+    {
+        if (id != request.Id)
+        {
+            return BadRequest($"Route parameter does not match with body parameter: {request.Id}");
+        }
+
+        await _service.UpdateServiceGroupAsync(request);
+
+        return Ok();
     }
 }
