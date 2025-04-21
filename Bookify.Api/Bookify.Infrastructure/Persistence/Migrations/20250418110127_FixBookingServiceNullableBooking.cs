0c185dfa815757b5ed6bb9e6d12da2864965fca7
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-04-21 17:58:35 +0500

    add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250418110127_FixBookingServiceNullableBooking.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBookingServiceNullableBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking",
+                column: "ServiceId",
+                principalTable: "Service",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Service_ServiceId",
+                table: "Booking",
+                column: "ServiceId",
+                principalTable: "Service",
+                principalColumn: "Id");
+        }
+    }
+}
