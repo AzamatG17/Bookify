c08108abde1f35610b985eac1b93b90dea9f884f
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-05-01 17:59:05 +0500

    Update Dtos

---
+++ Bookify.Api/Bookify.Application/DTOs/ServiceWithRatingDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookify.Application.DTOs;
+
+public record ServiceWithRatingDto(
+    int Id,
+    int ServiceId,
+    string ServiceName,
+    string Color,
+    int CompanyId,
+    string CompanyName,
+    int BranchId,
+    string BranchName,
+    double? Rating,
+    double? CoordinateLatitude,
+    double? CoordinateLongitude);
