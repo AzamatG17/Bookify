1d1ecb63a1af3a7916004fd3598379e1df163e13
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-05-07 17:56:11 +0500

    add migrations

---
+++ Bookify.Api/Bookify.Infrastructure/Persistence/Migrations/20250506050109_Add-IsActive.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ServiceTranslation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ServiceRating",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ServiceGroupTranslation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ServiceGroup",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Service",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "OpeningTimeBranch",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "ETicket",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Companies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Branch",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Booking",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ServiceTranslation");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ServiceRating");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ServiceGroupTranslation");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ServiceGroup");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Service");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "OpeningTimeBranch");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "ETicket");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Branch");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Booking");
+        }
+    }
+}
