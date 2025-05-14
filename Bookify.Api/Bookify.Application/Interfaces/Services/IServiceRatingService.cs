9abb4b373e7f7f9a9fb63085b5a507e6e9dbce41
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-05-14 09:56:42 +0500

    minor update

--- Bookify.Api/Bookify.Application/Interfaces/Services/IServiceRatingService.cs
+++ Bookify.Api/Bookify.Application/Interfaces/Services/IServiceRatingService.cs
@@ -1,10 +1,12 @@
 ﻿using Bookify.Application.DTOs;
+using Bookify.Application.QueryParameters;
 
 namespace Bookify.Application.Interfaces.Services;
 
 public interface IServiceRatingService
 {
     Task<List<ServiceRatingDto>> GetAllServiceRatingAsync();
+    Task<List<ServiceRatingByCompanyDto>> GetAllServiceRatingByCompanyAsync(ServiceRatingByCompanyQueryParametrs queryParametrs);
     Task<List<ServiceRatingDto>> GetServiceRatingAsync();
     Task<ServiceRatingDto> CreateServiceRatingAsync(ServiceRatingForCreateDto serviceRatingForCreateDto);
     Task UpdateAsync(ServiceRatingForUpdateDto dto);
