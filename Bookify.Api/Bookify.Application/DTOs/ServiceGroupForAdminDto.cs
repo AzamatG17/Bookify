c86c7f4fbacdddfc080b04551ce432b5be4a659d
Azamat Giyosov <azamatgiasov04@gmail.com>
2025-05-16 14:43:27 +0500

    minor update

---
+++ Bookify.Api/Bookify.Application/DTOs/ServiceGroupForAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookify.Application.DTOs;
+
+public record ServiceGroupForAdminDto(
+    int Id,
+    string Color,
+    string Name,
+    int CompanyId,
+    string CompanyName,
+    List<ServiceWithRatingDto> ServiceWithRatingDtos
+    );
